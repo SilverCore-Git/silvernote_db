b0eb2f7a545eb34af498baa8e7873d429402b1ea
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-10-14 09:01:19 +0200

    add routes

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,19 @@ import httpServer from "httpServer";
 
 import AuthMiddleware from "./middleware/Auth.js";
 
+import push from "./routes/push";
+import delete from "./routes/delete";
+import get from "./routes/get";
+
 const app = express();
 
 
 app.use(AuthMiddleware);
 
 // /push /delete /get
-
+app.use('/push', push);
+app.use('/delete', delete);
+app.use('/get', get);
 
 
 
