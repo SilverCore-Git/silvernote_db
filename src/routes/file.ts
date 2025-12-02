648fde4656c6405805d6d72a3817fd58c2211372
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-12-02 19:26:33 +0100

    fix file.ts

--- src/routes/file.ts
+++ src/routes/file.ts
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
         cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
-        cb(null, file.filename);
+        cb(null, file.originalname);
     }
 });
 
